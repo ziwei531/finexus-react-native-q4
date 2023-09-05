ab0a16a448b55dff35da06dcfefa0a8546f08f5f
unknown <ziwei531@gmail.com>
2023-09-05 15:21:31 +0800

    add shuffling

--- App.tsx
+++ App.tsx
@@ -18,6 +18,8 @@ import {
   Modal,
   Pressable,
   Alert,
+  PixelRatio,
+  FlatList,
 } from 'react-native';
 
 import {
@@ -29,36 +31,132 @@ import {
 } from 'react-native/Libraries/NewAppScreen';
 
 function App(): JSX.Element {
+  const colors = [
+    'lightseagreen',
+    'firebrick',
+    'lightpink',
+    'maroon',
+    'cornflowerblue',
+    'burlywood',
+    'darkslateblue',
+    'lightcoral',
+    'orange',
+    'darksalmon',
+  ];
+
+  // this is to get the font size based on the device's font scale. it makes the fonts responsive.
+  const fontScale = PixelRatio.getFontScale();
+  const getFontSize = (size: number) => size / fontScale;
+
   const [milliseconds, setMilliseconds] = useState<number>(0);
   const [isStart, setIsStart] = useState<boolean>(false);
+
+  // modals' useState variables
   const [startModalIsVisible, setStartModalIsVisible] =
     useState<boolean>(false);
-  const [color, setColor] = useState<string>(randomColor());
-  const [arrColors, setArrColors] = useState<string[]>([]);
-  const [highestRecord, setHighestRecord] = useState<number>(0);
+  const [highestRecordModalIsVisible, setHighestRecordModalIsVisible] =
+    useState<boolean>(false);
 
-  // this is where the color changing logic is
+  // the required color that the user needs to click.
+  const [requiredColor, setRequiredColor] = useState<string>('');
+
+  const [arrColors, setArrColors] = useState<string[]>([]);
+
+  //when one of the records were pressed in the highest record modal, this keeps track of the record that was pressed.
+
+  // when the user is playing the game, this keeps track of their record.
+  const [record, setRecord] = useState<number>(0);
+
+  // dummy data will be used for testing purposes.
+  const DUMMY_DATA = [];
+
+  for (let i = 0; i < 20; i++) {
+    DUMMY_DATA.push({key: i + 1, record: Math.floor(Math.random() * 1000)});
+  }
+
+  // when the user is checking the highest record, this keeps track of the highest record. It shall be stored in an array.
+  const [highestRecords, setHighestRecords] = useState<
+    Array<{key: number; record: number}>
+  >([]);
+
+  const fourBlockColors = (requiredColor: string) => {
+    /*
+    Ensure that this function is executed when start modal is executed so that the requiredColor can be generated. 
+
+    when the game is initialized, the 4 blocks of color should have been given based on randomColor()'s given result and be remained as such throughout the game. 
+    the requiredColor must be within one of the 4 blocks of color. 
+    the requiredColor must alternate between the 4 blocks of color. 
+
+    */
+
+    const colors = [
+      'lightseagreen',
+      'firebrick',
+      'lightpink',
+      'maroon',
+      'cornflowerblue',
+      'burlywood',
+      'darkslateblue',
+      'lightcoral',
+      'orange',
+      'darksalmon',
+    ];
+
+    if (colors.includes(requiredColor)) {
+      const filteredColors = colors.filter(color => color !== requiredColor);
+
+      const randomizeColor = randomColor(filteredColors);
+
+      setArrColors([
+        requiredColor,
+        randomizeColor.randomItem(),
+        randomizeColor.randomItem(),
+        randomizeColor.randomItem(),
+      ]);
+    }
+  };
+
+  const shuffleArray = (array: string[]) => {
+    for (let i = array.length - 1; i > 0; i--) {
+      const j = Math.floor(Math.random() * (i + 1));
+      [array[i], array[j]] = [array[j], array[i]];
+    }
+  };
+
+  const shuffleTheColors = (array: string[], index: number) => {
+    for (let i = array.length - 1; i > 0; i--) {
+      const j = Math.floor(Math.random() * (i + 1));
+      [array[index], array[j]] = [array[j], array[index]];
+    }
+  };
+
+  // timer and color blocks constantly changing logic
   useEffect(() => {
-    let intervalId: NodeJS.Timeout;
+    let intervalColors: NodeJS.Timeout;
+    let intervalTimer: NodeJS.Timeout;
+
+    console.log(arrColors);
 
     if (isStart) {
-      intervalId = setInterval(() => {
-        setArrColors([
-          randomColor(),
-          randomColor(),
-          randomColor(),
-          randomColor(),
-        ]);
-      }, 500);
+      intervalColors = setInterval(() => {
+        shuffleTheColors(arrColors, arrColors.indexOf(requiredColor));
+      }, 400);
+
+      intervalTimer = setInterval(() => {
+        setMilliseconds(currentTime => currentTime + 1);
+      }, 100);
     }
 
     return () => {
-      clearInterval(intervalId);
+      clearInterval(intervalColors);
+      clearTimeout(intervalTimer);
     };
   }, [isStart]);
 
+  // this is triggered to check what the user has chosen between starting the game or cancelling it.
   const handleModalChoice = (status: string) => {
     if (status === 'Start') {
+      fourBlockColors(requiredColor);
       setIsStart(true);
       setStartModalIsVisible(false);
     } else {
@@ -71,7 +169,10 @@ function App(): JSX.Element {
     if (isStart) {
       return;
     }
-    setColor(randomColor());
+
+    const randomizeColor = randomColor(colors);
+
+    setRequiredColor(randomizeColor.randomItem());
     setStartModalIsVisible(true);
   };
 
@@ -81,13 +182,30 @@ function App(): JSX.Element {
     if (isStart) {
       return;
     }
+
+    if (highestRecordModalIsVisible) {
+      setHighestRecordModalIsVisible(false);
+      return;
+    }
+
+    setHighestRecordModalIsVisible(true);
   };
 
-  // alert result
+  // adding the new record handler
+  const addNewRecord = (newRecord: number) => {
+    setHighestRecords(currentRecords => [
+      ...currentRecords,
+      {key: Math.random(), record: newRecord},
+    ]);
+  };
+
+  // alert result. this is triggered when the user clicks on one of the color blocks. will also stop the game.
   const alertResult = (chosenColor: string) => {
-    //for now, set it to false as temporary measure.
     setIsStart(false);
-    if (chosenColor === color) {
+
+    if (chosenColor === requiredColor) {
+      addNewRecord(milliseconds);
+
       Alert.alert(
         'Congratulations!',
         `You had used ${milliseconds} to click ${chosenColor} color.`,
@@ -96,17 +214,19 @@ function App(): JSX.Element {
     } else {
       Alert.alert('Sorry', `This is ${chosenColor}`, [{text: 'OK'}]);
     }
+
+    setMilliseconds(0);
   };
 
   return (
     <View style={[styles.parentContainer]}>
-      {/* Top Part */}
+      {/* Top Part. Timer and Highest Record Display */}
       <View style={styles.topPart}>
         <View>
           <Text
             style={{
               color: 'white',
-              fontSize: 25,
+              fontSize: getFontSize(20),
               textAlign: 'center',
               backgroundColor: 'maroon',
               padding: 32,
@@ -114,8 +234,6 @@ function App(): JSX.Element {
             Timer: {milliseconds} milliseconds
           </Text>
         </View>
-
-        {/* Highest Record. Pressable */}
 
         <Pressable
           onPress={handleOpenHighestRecord}
@@ -130,13 +248,82 @@ function App(): JSX.Element {
               color: 'black',
               fontStyle: 'italic',
               fontWeight: 'bold',
-              fontSize: 25,
+              fontSize: getFontSize(20),
               textAlign: 'center',
             }}>
             Highest Record
           </Text>
         </Pressable>
+        {/* End of Highest Record and Timer Display */}
+
+        {/*  Modal Highest Record. */}
+        <Modal
+          transparent={true}
+          visible={highestRecordModalIsVisible}
+          animationType="slide">
+          <View style={styles.modalContainer}>
+            <View style={[styles.modalView, styles.modalViewHighestRecords]}>
+              <Text
+                style={{
+                  fontWeight: 'bold',
+                  fontSize: getFontSize(15),
+                  color: 'black',
+                  textAlign: 'center',
+                }}>
+                Highest Score
+              </Text>
+
+              {highestRecords.length === 0 ? (
+                <View>
+                  <Text
+                    style={{
+                      fontWeight: 'bold',
+                      fontSize: getFontSize(18),
+                      color: 'black',
+                      padding: 45,
+                      textAlign: 'center',
+                    }}>
+                    No records yet
+                  </Text>
+                </View>
+              ) : (
+                <FlatList
+                  data={highestRecords.sort((a, b) => a.record - b.record)}
+                  renderItem={({item}) => (
+                    <View
+                      style={[
+                        {
+                          backgroundColor:
+                            item === highestRecords[0]
+                              ? 'lightgrey'
+                              : 'transparent',
+                          borderRadius: 15,
+                        },
+                        {padding: 5, marginTop: 10, width: '100%'},
+                      ]}>
+                      <Text style={{fontSize: getFontSize(15), color: 'black'}}>
+                        {`${item.record} ms`}
+                      </Text>
+                    </View>
+                  )}
+                />
+              )}
+
+              <Pressable
+                style={({pressed}) => [
+                  pressed && styles.pressedItem,
+                  styles.closeButton,
+                ]}
+                onPress={handleOpenHighestRecord}>
+                <Text style={{textAlign: 'center', fontSize: getFontSize(15)}}>
+                  Close
+                </Text>
+              </Pressable>
+            </View>
+          </View>
+        </Modal>
       </View>
+      {/* End of HighestRecord Modal */}
 
       {/* Start Game Modal */}
       <Modal
@@ -146,13 +333,17 @@ function App(): JSX.Element {
         <View style={[styles.modalContainer]}>
           <View style={styles.modalView}>
             <View style={{paddingBottom: 10}}>
-              <Text style={{fontSize: 24, color: 'black'}}>Start Playing!</Text>
+              <Text style={{fontSize: getFontSize(20), color: 'black'}}>
+                Start Playing!
+              </Text>
             </View>
             <View style={{}}>
-              <Text style={{fontSize: 21, color: 'black'}}>
+              <Text style={{fontSize: getFontSize(15), color: 'black'}}>
                 Tap the{'    '}
-                <View style={{backgroundColor: color, padding: 8}}>
-                  <Text style={{fontSize: 21, color: 'white'}}>{color}</Text>
+                <View style={{backgroundColor: requiredColor, padding: 8}}>
+                  <Text style={{fontSize: getFontSize(20), color: 'white'}}>
+                    {requiredColor}
+                  </Text>
                 </View>
                 {'    '}
                 color.
@@ -186,8 +377,9 @@ function App(): JSX.Element {
           </View>
         </View>
       </Modal>
+      {/* End of Start Game Modal */}
 
-      {/* Middle Part */}
+      {/* Middle Part. Color Blocks Appears Here. */}
       {isStart && (
         <View style={styles.colorContainer}>
           <Pressable onPress={() => alertResult(arrColors[0])}>
@@ -241,59 +433,38 @@ function App(): JSX.Element {
         <Text
           style={{
             color: 'black',
-            fontSize: 32,
+            fontSize: getFontSize(20),
             textAlign: 'center',
             fontWeight: 'bold',
           }}>
           Start Game
         </Text>
       </Pressable>
+      {/* End of Bottom Part */}
     </View>
   );
 }
 
-const randomColorGenerator = (() => {
-  const colors = [
-    'lightseagreen',
-    'firebrick',
-    'lightpink',
-    'maroon',
-    'cornflowerblue',
-    'burlywood',
-    'darkslateblue',
-    'lightcoral',
-    'orange',
-    'darksalmon',
-  ];
+const randomColor = (colors: string[]) => {
+  let data = [...colors];
+  let chosenColors: string[] = [];
 
-  //randomize the array on every new execution
-  const shuffleArray = (array: string[]) => {
-    for (let i = array.length - 1; i > 0; i--) {
-      const j = Math.floor(Math.random() * (i + 1));
-      [array[i], array[j]] = [array[j], array[i]];
-    }
-  };
-
-  shuffleArray(colors);
-  let availableColors = [...colors];
-  let currentIndex = 0;
-
-  return () => {
-    //if reached the end already, assign colors the beginning again
-    if (currentIndex >= availableColors.length) {
-      availableColors = [...colors];
-      currentIndex = 0;
+  const getRandomColor = () => {
+    if (data.length === 0) {
+      data = chosenColors;
+      chosenColors = [];
     }
 
-    const selectedColor = availableColors[currentIndex];
-    currentIndex++;
-    return selectedColor;
-  };
-})();
+    const index = Math.floor(Math.random() * data.length);
+    const chosenColor = data.splice(index, 1)[0];
 
-// this will return a random unjiquej color.
-const randomColor = () => {
-  return randomColorGenerator();
+    chosenColors.push(chosenColor);
+    return chosenColor;
+  };
+
+  return {
+    randomItem: getRandomColor,
+  };
 };
 
 const styles = StyleSheet.create({
@@ -311,6 +482,14 @@ const styles = StyleSheet.create({
     backgroundColor: 'lightblue',
   },
 
+  closeButton: {
+    padding: 15,
+    width: '50%',
+    borderRadius: 30,
+    backgroundColor: '#AA336A',
+    marginTop: 10,
+  },
+
   pressedItem: {
     backgroundColor: '#C3B0A2',
   },
@@ -320,7 +499,8 @@ const styles = StyleSheet.create({
     flexWrap: 'wrap',
     justifyContent: 'center',
     alignItems: 'center',
-    paddingBottom: 130,
+    alignContent: 'center',
+    paddingBottom: 100,
   },
 
   modalContainer: {
@@ -328,6 +508,12 @@ const styles = StyleSheet.create({
     flex: 1,
     justifyContent: 'center',
   },
+
+  modalViewHighestRecords: {
+    alignItems: 'center',
+    height: '30%',
+  },
+
   modalView: {
     margin: 40,
     borderRadius: 35,
